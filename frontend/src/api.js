36ceb1727b6cb3508150a983fe65bdf746465c8b
phaneendra999 <phanindrasangoju999@gmail.com>
2026-03-31 03:58:32 +0530

    Use env var for API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:8080' });
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080' });
 
 export const getUsers = () => api.get('/users');
 export const getUsersSummary = () => api.get('/users/summary');
